ad0efc8f5a843bd988d9e29b6da8867a70a68110
2eronia <3erino+github+2eronia@gmail.com>
2019-07-11 05:56:11 +0800

    Errors and Exception Handling

---
+++ SoloLearn/ErrorsAndExceptionsHomework.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created by serin at 7/11/2019
+"""
+
+'''
+def ask_for_int():
+	while True:
+		try:
+			n = int(input('Please input a num: '))
+		except:
+			print('Try again')
+			continue
+		else:
+			print(f'Your num squared is {n**2}')
+			break
+'''
+
+def ask_for_int():
+	try:
+		n = int(input('Please input a num: '))
+	except:
+		print('Try again')
+		ask_for_int()
+	else:
+		print(f'Your num squared is {n**2}')
+
+ask_for_int()
