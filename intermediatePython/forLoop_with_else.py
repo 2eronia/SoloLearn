df5ab2e3febeecc13cc1ab6f0e2b79bec10dcb45
2eronia <3erino+github+2eronia@gmail.com>
2019-12-20 23:58:12 +0800

    Errors and Exception Handling

--- intermediatePython/forLoop_with_else.py
+++ intermediatePython/forLoop_with_else.py
@@ -2,17 +2,24 @@
 """
 Created by AnonymouS at 12/6/2019
 """
+
 import pysnooper
+import time
 
 
 # @pysnooper.snoop()
 def yield_prime_nums(num):
-	for i in range(2, num+1):
+	for i in range(2, num + 1):
 		for j in range(2, i // 2):
 			if i % j == 0:
 				break
 		else:
 			yield i
 
+
 input_num = int(input("input a number:\n"))
+t0 = time.time()
 prime_nums_output = print(list(yield_prime_nums(input_num)))
+
+t1 = time.time()
+print('Time required:', t1 - t0)
